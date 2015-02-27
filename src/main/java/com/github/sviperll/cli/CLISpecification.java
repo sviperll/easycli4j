d182124dd240099e811a3b4e41ad0a4976aa6cdf
Victor Nazarov <asviraspossible@gmail.com>
2015-02-27 17:16:05 +0300

    Major update.
    Migrate code from chicory metapackage to stand-alone project.

--- src/main/java/com/github/sviperll/cli/CLISpecification.java
+++ src/main/java/com/github/sviperll/cli/CLISpecification.java
@@ -32,70 +32,153 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
+/**
+ * Facade class to provide command line argument parsing functionality
+ *
+ *  * `CLISpecification` instance is set up with `add` methods
+ *  * `run` method is called and all handlers are called with right arguments as a result
+ *
+ * If some of handlers throws an exception then `CLIException` is thrown with correct error message
+ *
+ * @see CLISpecification#add
+ * @see CLISpecification#run
+ * @see CLIParameterFormatException
+ *
+ * @author Victor Nazarov <asviraspossible@gmail.com>
+ */
 public class CLISpecification {
+    private static final ParameterDescriptionFormatter DEFAULT_FORMATTER = new DefaultParameterDescriptionFormatter();
+
     private final Map<String, String> usage = new TreeMap<String, String>();
     private final Map<Character, CLIFlagHandler> shortFlagHandlers = new TreeMap<Character, CLIFlagHandler>();
     private final Map<String, CLIFlagHandler> longFlagHandlers = new TreeMap<String, CLIFlagHandler>();
     private final Map<Character, CLIParameterHandler> shortParameterHandlers = new TreeMap<Character, CLIParameterHandler>();
     private final Map<String, CLIParameterHandler> longParameterHandlers = new TreeMap<String, CLIParameterHandler>();
+    private final Appendable usageWriter;
+    private final ParameterDescriptionFormatter formatter;
 
+    /**
+     * Constucts a new instance of CLISpecification
+     *
+     * When you call `usage` method a usage-message is written into `usageWriter`
+     *
+     * @param usageWriter an `Appendable`, usually `System.out`
+     * @param formatter to combine description with notice about default value in usage-message
+     *
+     * @see #usage
+     * @see ParameterDescriptionFormatter
+     */
+    public CLISpecification(Appendable usageWriter, ParameterDescriptionFormatter formatter) {
+        this.usageWriter = usageWriter;
+        this.formatter = new ParameterDescriptionFormatterNullWrapper(formatter);
+    }
+
+    /**
+     * Constucts a new instance of CLISpecification
+     *
+     * When you call `usage` method a usage-message is written into `usageWriter`
+     *
+     * @param usageWriter an `Appendable`, usually `System.out`
+     * @see #usage
+     */
+    public CLISpecification(Appendable usageWriter) {
+        this(usageWriter, DEFAULT_FORMATTER);
+    }
+
+    /**
+     * Add a command line key with short and long form and without arguments
+     *
+     * @param c short, i. e. single-letter form of a key
+     * @param s lone, i. e. any string form of a key
+     * @param description description for usage-message
+     * @param handler key handler
+     *
+     * @see CLIFlagHandler
+     */
     public void add(char c, String s, String description, CLIFlagHandler handler) {
-        newFlagOpt(c, description);
-        newFlagOpt(s, description);
+        addFlagUsageEntry(c, description);
+        addFlagUsageEntry(s, description);
         shortFlagHandlers.put(c, handler);
         longFlagHandlers.put(s, handler);
     }
+
+    /**
+     * Add a command line key without short form and without arguments
+     *
+     * @param s lone, i. e. any string form of a key
+     * @param description description for usage-message
+     * @param handler key handler
+     *
+     * @see CLIFlagHandler
+     */
     public void add(String s, String description, CLIFlagHandler handler) {
-        newFlagOpt(s, description);
+        addFlagUsageEntry(s, description);
         longFlagHandlers.put(s, handler);
     }
+
+    /**
+     * Add a command line key with short and long form and with argument
+     *
+     * @param c short, i. e. single-letter form of a key
+     * @param s lone, i. e. any string form of a key
+     * @param description description for usage-message
+     * @param handler key handler
+     *
+     * @see CLIFlagHandler
+     */
     public void add(char c, String s, String description, CLIParameterHandler handler) {
-        newParamOpt(c, description);
-        newParamOpt(s, description);
+        addParameterUsageEntry(c, formatter.formatParameterDescription(description, handler.getDefaultValue()));
+        addParameterUsageEntry(s, formatter.formatParameterDescription(description, handler.getDefaultValue()));
         shortParameterHandlers.put(c, handler);
         longParameterHandlers.put(s, handler);
     }
+
+    /**
+     * Add a command line key with only long form and with argument
+     *
+     * @param s lone, i. e. any string form of a key
+     * @param description description for usage-message
+     * @param handler key handler
+     *
+     * @see CLIFlagHandler
+     */
     public void add(String s, String description, CLIParameterHandler handler) {
-        newParamOpt(s, description);
+        addParameterUsageEntry(s, formatter.formatParameterDescription(description, handler.getDefaultValue()));
         longParameterHandlers.put(s, handler);
     }
 
-    private void newFlagOpt(char c, String description) {
+    private void addFlagUsageEntry(char c, String description) {
         String s = new String(new char[] {c});
         if (usage.put(s, "\t\t -" + s + "\t\t" + description) != null)
             throw new IllegalArgumentException("Option " + s + " already defined!");
     }
 
-    private void newFlagOpt(String s, String description) {
+    private void addFlagUsageEntry(String s, String description) {
         if (usage.put(s, "\t\t--" + s + "\t\t" + description) != null)
             throw new IllegalArgumentException("Option " + s + " already defined!");
     }
 
-    private void newParamOpt(char c, String description) {
+    private void addParameterUsageEntry(char c, String description) {
         String s = new String(new char[] {c});
         if (usage.put(s, "\t\t -" + s + " OPTION\t" + description) != null)
             throw new IllegalArgumentException("Option " + s + " already defined!");
     }
 
-    private void newParamOpt(String s, String description) {
+    private void addParameterUsageEntry(String s, String description) {
         if (usage.put(s, "\t\t--" + s + "=OPTION\t" + description) != null)
             throw new IllegalArgumentException("Option " + s + " already defined!");
     }
 
     public String[] run(String[] args) throws CLIException {
         List<String> unprocessed = new ArrayList<String>();
-        Parser p = new Parser(args);
+        Processor p = new Processor(new Parser(args));
         for (;;) {
             if (p.isEndOfArgs()) {
                 break;
-            } else if (p.current().equals("-")) {
-                unprocessed.add(p.current());
-                p.next();
-            } else if (p.current().startsWith("-")) {
-                if (p.current().startsWith("--"))
-                    processLongOpts(p);
-                else
-                    processShortOpts(p);
+            } else if (p.isLongOption()) {
+                p.processLongOption();
+            } else if (p.isShortOptions()) {
+                p.processShortOptions();
             } else {
                 unprocessed.add(p.current());
                 p.next();
@@ -105,79 +188,155 @@ public class CLISpecification {
         return unprocessed.toArray(res);
     }
 
-    public void usage(Appendable pw) throws IOException {
+    /**
+     * Write usage-message to a writer given when constructing CLISpecification instance
+     * @throws IOException
+     */
+    public void usage() throws IOException {
         for (Map.Entry<String, String> e: usage.entrySet()) {
-            pw.append(e.getValue());
-            pw.append("\n");
+            usageWriter.append(e.getValue());
+            usageWriter.append("\n");
         }
     }
 
-    private void processLongOpts(Parser p) throws CLIException {
-        for (Map.Entry<String, CLIFlagHandler> e: longFlagHandlers.entrySet()) {
-            if (p.current().equals("--" + e.getKey())) {
-                e.getValue().handleCLIFlag();
-                p.next();
-                return;
-            }
-        }
-        for (Map.Entry<String, CLIParameterHandler> e: longParameterHandlers.entrySet()) {
-            String option = "--" + e.getKey();
-            String prefix = option + "=";
-            if (p.current().startsWith(prefix)) {
-                try {
-                    e.getValue().handleCLIParameter(p.current().substring(prefix.length()));
-                } catch (CLIParameterFormatException ex) {
-                    throw new CLIException("Error reading option: " + option + ": " + ex.getMessage(), ex);
-                }
-                p.next();
-                return;
-            } else if (p.current().equals(option)) {
-                p.next();
-                try {
-                    e.getValue().handleCLIParameter(p.readParameter(e.getKey()));
-                } catch (CLIParameterFormatException ex) {
-                    throw new CLIException("Error reading option: " + option + ": " + ex.getMessage(), ex);
-                }
-                return;
-            }
-        }
-        throw new CLIException("Unknown option " + p.current());
+    /**
+     * Interface to specify how to write default value of some command line key in usage-message
+     * 
+     * @see ParameterDescriptionFormatter#formatParameterDescription
+     */
+    public interface ParameterDescriptionFormatter {
+        /**
+         * Defines how to write default value of some command line key in usage-message
+         *
+         * For example, a call to
+         *
+         *     formatParameterDescription("number of pages", "2")
+         *
+         * may result in
+         *
+         *     "number of pages (default 2)"
+         *
+         * string to be written in usage-message
+         *
+         * @param description a description of command line key provided with CLISpecification#add method
+         * @param defaultValue a default value of given comman line parameter
+         * @return a compond message with the original description and an notice abount default value
+         */
+        String formatParameterDescription(String description, String defaultValue);
     }
 
-    private void processShortOpts(Parser p) throws CLIException {
-        char[] c = p.current().toCharArray();
-        Map.Entry<Character, CLIParameterHandler> handler = null;
-        for (int i = 1; i < c.length; i++) {
-            boolean found = false;
-            for (Map.Entry<Character, CLIFlagHandler> e: shortFlagHandlers.entrySet()) {
-                if (e.getKey().equals(c[i])) {
+    private static class DefaultParameterDescriptionFormatter implements ParameterDescriptionFormatter {
+        @Override
+        public String formatParameterDescription(String description, String defaultValue) {
+            return defaultValue == null ? description : description + " (default " + defaultValue + ")";
+        }
+    }
+
+    private static class ParameterDescriptionFormatterNullWrapper implements ParameterDescriptionFormatter {
+        private final ParameterDescriptionFormatter formatter;
+        ParameterDescriptionFormatterNullWrapper(ParameterDescriptionFormatter formatter) {
+            this.formatter = formatter;
+        }
+
+        @Override
+        public String formatParameterDescription(String description, String defaultValue) {
+            return defaultValue == null ? description : formatter.formatParameterDescription(description, defaultValue);
+        }
+    }
+
+    private class Processor {
+        private final Parser parser;
+        Processor(Parser parser) {
+            this.parser = parser;
+        }
+
+        private boolean isEndOfArgs() {
+            return parser.isEndOfArgs();
+        }
+
+        private String current() throws CLIException {
+            return parser.current();
+        }
+
+        private void next() {
+            parser.next();
+        }
+
+        private boolean isLongOption() throws CLIException {
+            return current().startsWith("--");
+        }
+
+        private boolean isShortOptions() throws CLIException {
+            return current().startsWith("-") && !current().equals("-") && !isLongOption();
+        }
+
+        private void processLongOption() throws CLIException {
+            for (Map.Entry<String, CLIFlagHandler> e: longFlagHandlers.entrySet()) {
+                if (parser.current().equals("--" + e.getKey())) {
                     e.getValue().handleCLIFlag();
-                    found = true;
+                    parser.next();
+                    return;
                 }
             }
-            for (Map.Entry<Character, CLIParameterHandler> e: shortParameterHandlers.entrySet()) {
-                if (e.getKey().equals(c[i])) {
-                    if (handler != null)
-                        throw new CLIException("Expecting parameter for -" + handler.getKey());
-                    else
-                        handler = e;
-                    found = true;
+            for (Map.Entry<String, CLIParameterHandler> e: longParameterHandlers.entrySet()) {
+                String option = "--" + e.getKey();
+                String prefix = option + "=";
+                if (parser.current().startsWith(prefix)) {
+                    try {
+                        e.getValue().handleCLIParameter(parser.current().substring(prefix.length()));
+                    } catch (CLIParameterFormatException ex) {
+                        throw new CLIException("Error reading option: " + option + ": " + ex.getMessage(), ex);
+                    }
+                    parser.next();
+                    return;
+                } else if (parser.current().equals(option)) {
+                    parser.next();
+                    try {
+                        e.getValue().handleCLIParameter(parser.readParameter(e.getKey()));
+                    } catch (CLIParameterFormatException ex) {
+                        throw new CLIException("Error reading option: " + option + ": " + ex.getMessage(), ex);
+                    }
+                    return;
                 }
             }
-            if (!found)
-                throw new CLIException("Unknown option -" + c[i]);
+            throw new CLIException("Unknown option " + parser.current());
         }
-        if (handler != null) {
-            p.next();
-            try {
-                handler.getValue().handleCLIParameter(p.readParameter(handler.getKey()));
-            } catch (CLIParameterFormatException ex) {
-                throw new CLIException("Error reading option: -" + handler.getKey() + ": " + ex.getMessage(), ex);
+
+        private void processShortOptions() throws CLIException {
+            char[] c = parser.current().toCharArray();
+            Map.Entry<Character, CLIParameterHandler> handler = null;
+            for (int i = 1; i < c.length; i++) {
+                boolean found = false;
+                for (Map.Entry<Character, CLIFlagHandler> e: shortFlagHandlers.entrySet()) {
+                    if (e.getKey().equals(c[i])) {
+                        e.getValue().handleCLIFlag();
+                        found = true;
+                    }
+                }
+                for (Map.Entry<Character, CLIParameterHandler> e: shortParameterHandlers.entrySet()) {
+                    if (e.getKey().equals(c[i])) {
+                        if (handler != null)
+                            throw new CLIException("Expecting parameter for -" + handler.getKey());
+                        else
+                            handler = e;
+                        found = true;
+                    }
+                }
+                if (!found)
+                    throw new CLIException("Unknown option -" + c[i]);
+            }
+            if (handler != null) {
+                parser.next();
+                try {
+                    handler.getValue().handleCLIParameter(parser.readParameter(handler.getKey()));
+                } catch (CLIParameterFormatException ex) {
+                    throw new CLIException("Error reading option: -" + handler.getKey() + ": " + ex.getMessage(), ex);
+                }
             }
         }
     }
 
-    private class Parser {
+    private static class Parser {
 
         private final String[] args;
         private int i;
@@ -193,7 +352,7 @@ public class CLISpecification {
 
         public String current() throws CLIException {
             if (!(i < args.length))
-                throw new CLIException("Expection argument, got end of line");
+                throw new CLIException("Expecting argument, got end of line");
             return args[i];
         }
 
